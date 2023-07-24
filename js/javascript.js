06a140f170abc7e098d1b36010c4ad2e9b8c9d0b
Nail003 <danialzafarstudy003@gmail.com>
2023-07-24 21:29:02 +0500

    [FTR] expanded game() function to repeat games according to the number of rounds set

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,39 @@
 game();
 
-// Helper Functions
-
 function game() {
-    for (let i = 0; i < 5; i++) {
+    // Initial score and total rounds
+    let playerScore = 0;
+    let computerScore = 0;
+    let rounds = 3;
+
+    // Run game
+    for (let i = 0; i < rounds; i++) {
+        // Get player and computer inputs
         let playerChoice = prompt(
             "Type either 'rock', 'paper' or 'scissors' to make your choice"
         );
         let computerChoice = getComputerChoice();
+
+        // Check winner
         let gameResult = tellWinner(playerChoice, computerChoice);
+
+        // Display win-loss message
         console.log(gameResult.message);
+
+        // Update score
+        if (gameResult.result === "won") playerScore++;
+        if (gameResult.result === "loss") computerScore++;
     }
+
+    // Display score at the end
+    console.log(`%cPlayer score is ${playerScore}`, "color: green");
+    console.log(`%cComputer score is ${computerScore}`, "color: orange");
 }
 
+// Helper Functions
 function tellWinner(playerChoice, computerChoice) {
     playerChoice = playerChoice.toUpperCase();
+
     let winCondition1 =
         playerChoice === "ROCK" && computerChoice === "SCISSORS";
     let winCondition2 =
