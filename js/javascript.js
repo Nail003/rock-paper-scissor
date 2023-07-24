363d5c4b5bd726925d0bd6152bf30fbb17755b1f
Nail003 <danialzafarstudy003@gmail.com>
2023-07-24 21:17:09 +0500

    [FTR] add function game() that runs five rounds of game and also prompts user for his input

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,17 @@
-let playerChoice = "Rock";
-let computerChoice = getComputerChoice();
-
-console.log(tellWinner(playerChoice, computerChoice));
+game();
 
 // Helper Functions
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerChoice = prompt(
+            "Type either 'rock', 'paper' or 'scissors' to make your choice"
+        );
+        let computerChoice = getComputerChoice();
+        let gameResult = tellWinner(playerChoice, computerChoice);
+        console.log(gameResult.message);
+    }
+}
 
 function tellWinner(playerChoice, computerChoice) {
     playerChoice = playerChoice.toUpperCase();
@@ -15,16 +23,25 @@ function tellWinner(playerChoice, computerChoice) {
 
     // Player wins
     if (winCondition1 || winCondition2 || winCondition3) {
-        return `You won!! ${playerChoice} beats ${computerChoice}`;
+        return {
+            result: "won",
+            message: `You won!! ${playerChoice} beats ${computerChoice}`,
+        };
     }
 
     // For draw
     if (playerChoice === computerChoice) {
-        return `Draw!! Both choses ${playerChoice}`;
+        return {
+            result: "draw",
+            message: `Draw!! Both choses ${playerChoice}`,
+        };
     }
 
     // Player losses
-    return `You loss!! ${computerChoice} beats ${playerChoice}`;
+    return {
+        result: "loss",
+        message: `You loss!! ${computerChoice} beats ${playerChoice}`,
+    };
 }
 
 function getComputerChoice() {
