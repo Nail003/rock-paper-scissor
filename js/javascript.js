790aea90a43b72e66b1f08bea1cd823f4c40159e
Nail003 <danialzafarstudy003@gmail.com>
2023-07-24 20:47:41 +0500

    [FTR] add function tellWinner() to return message telling who won the rock paper scissors match

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,28 @@
+console.log(tellWinner("Rock", getComputerChoice()));
+
+// Helper Functions
+
+function tellWinner(playerChoice, computerChoice) {
+    let winCondition1 =
+        playerChoice === "Rock" && computerChoice === "Scissors";
+    let winCondition2 =
+        playerChoice === "Scissors" && computerChoice === "Paper";
+    let winCondition3 = playerChoice === "Paper" && computerChoice === "Rock";
+
+    // Player wins
+    if (winCondition1 || winCondition2 || winCondition3) {
+        return `You won!! ${playerChoice} beats ${computerChoice}`;
+    }
+
+    // For draw
+    if (playerChoice === computerChoice) {
+        return `Draw!! Both choses ${playerChoice}`;
+    }
+
+    // Player losses
+    return `You loss!! ${computerChoice} beats ${playerChoice}`;
+}
+
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
     return choices[randomNumber(0, 2)];
@@ -6,5 +31,3 @@ function getComputerChoice() {
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max + 1 - min) + min);
 }
-
-console.log(getComputerChoice());
